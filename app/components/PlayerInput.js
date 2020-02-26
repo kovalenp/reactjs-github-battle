f4b52651e2229a24068350801e8dbe6ed5836fa9
Pavel Kovalenko <pavel@kovalenko.cz>
2020-02-26 08:13:38 +0100

    WIP: added PlayerInput component and styling

---
+++ app/components/PlayerInput.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+class PlayerInput extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.onSubmit(this.state.username)
+  }
+
+  handleChange(event) {
+    event.preventDefault()
+    this.setState({ username: event.target.value })
+  }
+
+  render() {
+    return (
+      <form className='column player' onSubmit={this.handleSubmit}>
+        <label htmlFor='username' className='player-label'>
+          {this.props.label}
+        </label>
+        <div className='row player-inputs'>
+          <input
+            type='text'
+            id='username'
+            className='input-light'
+            placeholder='github username'
+            autoComplete='off'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button
+            type='submit'
+            className='btn dark-btn'
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+}
+
+export default PlayerInput;
+
+
