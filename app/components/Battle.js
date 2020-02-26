9753f6fdf15ccf9fff6dd40f70843b6e501b0a66
Pavel Kovalenko <pavel@kovalenko.cz>
2020-02-26 17:41:51 +0100

    Added Player preview, user cleanup handler

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,37 +1,48 @@
 import React from 'react'
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
 import PlayerInput from './PlayerInput'
-
-function Instructions() {
-  return (
-    <div className='instructions-container'>
-      <h1 className='center-text header-lg'>
-        Instructions
-      </h1>
-      <ol className='container-sm grid center-text battle-instructions'>
-        <li>
-          <h3 className='header-sm'>Enter two Github users</h3>
-          <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140} />
-        </li>
-        <li>
-          <h3 className='header-sm'>Battle</h3>
-          <FaFighterJet className='bg-light' color='#727272' size={140} />
-        </li>
-        <li>
-          <h3 className='header-sm'>See the winners</h3>
-          <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
-        </li>
-      </ol>
-    </div>
-  )
-}
+import Instructions from './Instructions'
+import PlayerPreview from './PlayerPreview'
 
 export default class Battle extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      playerOne: null,
+      playerTwo: null,
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
+  }
+
+  handleSubmit(id, player) {
+    this.setState({ [id]: player })
+  }
+
+  handleReset(id) {
+    this.setState({ [id]: null })
+  }
+
   render() {
+    const { playerOne, playerTwo } = this.state
+
     return (
       <React.Fragment>
         <Instructions />
-        <PlayerInput label='this label!' onSubmit={value => alert(`This is ${value}`)} />
+        <div className='players-container'>
+          <h1 className='center-text header-lg'>Players</h1>
+          <div className='row space-around'>
+            {(playerOne === null)
+              ? <PlayerInput label='Player One' onSubmit={(player) => this.handleSubmit('playerOne', player)} />
+              : <PlayerPreview label='Player One' onReset={() => this.handleReset('playerOne')} username={playerOne} />
+            }
+            {(playerTwo === null)
+              ? <PlayerInput label='Player Two' onSubmit={(player) => this.handleSubmit('playerTwo', player)} />
+              : <PlayerPreview label='Player Two' onReset={() => this.handleReset('playerTwo')} username={playerTwo} />
+            }
+          </div>
+        </div>
       </React.Fragment>
     )
   }
